4e8c9d54664b8162d110324b208185f897bade0e
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2025-05-11 22:50:00 +0300

    fix: debug bug

--- app/src/app/api/create-payment-intent/route.ts
+++ app/src/app/api/create-payment-intent/route.ts
@@ -6,6 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 });
 
 export const POST = async () => {
+  console.log('obj:', stripe);
+  console.log('value123:', process.env.STRIPE_SECRET_KEY);
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: 1099,
