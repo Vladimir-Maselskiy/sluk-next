acb6b713142fa650342b894d03ca5babde04b0a2
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2025-05-11 23:45:51 +0300

    fix: debug bug

--- app/src/app/api/create-payment-intent/route.ts
+++ app/src/app/api/create-payment-intent/route.ts
@@ -1,14 +1,10 @@
 import { NextResponse } from 'next/server';
-
-import Stripe from 'stripe';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+import stripe from '../../../../lib/stripe';
 
 export const POST = async () => {
-  console.log('obj:', stripe);
-  console.log('value123:', process.env.STRIPE_SECRET_KEY);
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 1099,
+      amount: 10099,
       currency: 'usd',
       payment_method_types: ['card'],
     });
