8ac9c140a8d35152282238a9f06175ad5a15ddec
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2025-06-04 01:43:05 +0300

    fix: vercel deploy error

--- app/src/utils/db.ts
+++ app/src/utils/db.ts
@@ -2,8 +2,10 @@
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI || '';
+const stripe = process.env.STRIPE_SECRET_KEY;
 
 console.log('MONGODB_URI', MONGODB_URI);
+console.log('stripe', stripe);
 
 export const connectToDatabase = async () => {
   if (mongoose.connection.readyState >= 1) return;
