6b168ab715bd260651728bebbaac53a964516bed
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2025-06-25 23:49:13 +0300

    fix: vercel deploy error

--- app/src/app/payment/page.tsx
+++ app/src/app/payment/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import { Payment } from '../components/Stripe/Payment/Payment';
 
 export default function Home() {
   return (
     <main>
-      <Payment />
+      <Suspense fallback={<div>Loading payment...</div>}>
+        <Payment />
+      </Suspense>
     </main>
   );
 }
