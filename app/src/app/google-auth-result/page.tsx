0203d4d77579c896f7556007ef22b43d228731f7
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2025-08-18 10:54:02 +0300

    fix: bug with useSearchParams

--- app/src/app/google-auth-result/page.tsx
+++ app/src/app/google-auth-result/page.tsx
@@ -1,8 +1,17 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default function GoogleAuthResult() {
+function GoogleAuthResult() {
   const searchParams = useSearchParams();
   const status = searchParams.get('status');
   return <div style={{ textTransform: 'capitalize' }}>{status}</div>;
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <GoogleAuthResult />
+    </Suspense>
+  );
 }
